46f6367fbc96f172aa904670dc447a25858286e7
RaghavendraGb <raghavendragb3@example.com>
2026-04-04 12:18:44 +0530

    Update site metadata and service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.galnv8e9nvc"
+    "revision": "0.i6k9bf4gp14"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
